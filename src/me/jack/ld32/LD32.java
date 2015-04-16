c25fbdc03ac069aca03102261a195dea005025f4
Jack Patrick <jackpatrickmail@gmail.com>
2015-04-16 20:26:29 +0100

    Initial commit.

---
+++ src/me/jack/ld32/LD32.java
@@ -0,0 +1,13 @@
+package me.jack.ld32;
+
+import uk.co.jdpatrick.JEngine.JEngine;
+
+/**
+ * Created by Jack on 16/04/2015.
+ */
+public class LD32 {
+
+    public static void main(String[] args){
+        JEngine.start(new LD32Game("NAME For LD32 - Theme:????"),800,600);
+    }
+}
---
+++ src/me/jack/ld32/LD32Game.java
@@ -0,0 +1,20 @@
+package me.jack.ld32;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * Created by Jack on 16/04/2015.
+ */
+public class LD32Game extends StateBasedGame{
+
+    public LD32Game(String name) {
+        super(name);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+
+    }
+}
